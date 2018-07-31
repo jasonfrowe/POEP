a9afb75c2ede76eda35a4d27cd3453de8077bafa
Jason Rowe <Jason.Rowe@ubishops.ca>
2018-07-31 12:21:13 -0400

    Adding arch for reading in necessary data files

--- scenesim.f90
+++ scenesim.f90
@@ -2,7 +2,23 @@ program scenesimulator
 !Code for generating FITS files given a PSF and point positions
 use precision
 implicit none
+integer :: xout, yout
+character(80) :: prefix,fileout,fileout_c,fileout_m,filename
 
+!Image Dimensions
+xout=1024
+yout=1024
+
+!Name for output
+prefix="sgen"
+fileout=trim(prefix)//".fits"
+fileout_m=trim(prefix)//"_m.txt"
+fileout_c=trim(prefix)//"_c.fits"
+
+!input file
+filename="scenemodel_20180731.dat"
+
+call readmodel(filename)
 
 
 end program scenesimulator
---
+++ utils/readmodel.f90
@@ -0,0 +1,10 @@
+subroutine readmodel(filename)
+use precision
+implicit none
+!input vars
+character(80) :: filename
+!local vars
+
+
+return
+end subroutine readmodel
